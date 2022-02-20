2a90b290c553bef4a3aa188e07d1fe8ed8265025
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-02-20 12:44:20 +0800

    initial Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './navbar.css';
+import { RiMenuFill, RiCloseLine } from 'react-icons/ri';
+
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#minting_now">Minting Now</a></p>
+    <p><a href="#upcoming">Upcoming</a></p>
+    <p><a href="#past">Past</a></p>
+    <p><a href="#about_us">About Us</a></p>
+    <p><a href="#create">Create</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className="verynear__navbar">
+      <div className="verynear__navbar-links">
+        <div className="verynear__navbar-links_logo">
+        </div>
+        <div className="verynear__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="verynear__navbar-sign">
+        <button type='button'>Sign In</button>
+      </div>
+      <div className="verynear__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenuFill color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="verynear__navbar-menu_container scale-up-center">
+            <div className="verynear__navbar-menu_container-links">
+              <Menu />
+              <div className="verynear__navbar-menu_container-links-sign">
+                <button type='button'>Sign In</button>
+              </div>
+            </div>
+          </div>
+        )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
