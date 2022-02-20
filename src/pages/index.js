052d0d703ce7bbc2d739caf7840738ffafdf207b
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-02-20 20:51:17 +0800

    export past page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './home/Home';
 export { default as MintingNow } from './minting-now/MintingNow';
 export { default as Upcoming } from './upcoming/Upcoming';
+export { default as Past } from './past/Past';
