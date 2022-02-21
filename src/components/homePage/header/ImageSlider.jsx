b022c6f06291d6c67071c8a1dc8ef90a8e95f846
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-02-21 20:47:40 +0800

    image slider

---
+++ src/components/homePage/header/ImageSlider.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './image-slider.css';
+import { SliderData } from './SliderData';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className='slider'>
+      <FaAngleLeft className='left-arrow' onClick={prevSlide} />
+      <FaAngleRight className='right-arrow' onClick={nextSlide} />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.image} alt='travel image' className='image' />
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default ImageSlider;
