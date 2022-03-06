9cd254693f1721ffe136eaa63a2a2d86b2c6553c
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-06 23:19:50 +0800

    nft image template

--- src/components/homePage/common-components/NFT/NFT.jsx
+++ src/components/homePage/common-components/NFT/NFT.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import './nft.css';
 
 const NFT = ({ imgUrl, date, text }) => (
-  <div className="very-near__blog-container_article">
-    <div className="very-near__blog-container_article-image">
-      <img src={imgUrl} alt="blog_image" />
+  <div className="very-near__nft">
+    <div className="very-near__nft-image">
+      <img src={imgUrl} alt="nft_image" />
     </div>
-    {/* <div className="very-near__blog-container_article-content">
-      <div>
-        <p>{date}</p>
-        <h3>{text}</h3>
-      </div>
-      <p>Read Full Article</p>
-    </div> */}
   </div>
 );
 
