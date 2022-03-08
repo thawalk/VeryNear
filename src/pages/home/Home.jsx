c92a694016f78d0e3f1f4998828b9c6df805e9b5
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-08 20:49:17 +0800

    import launch-steps

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/homePage/header/Header";
 import MintingNow from "../../components/homePage/minting-now/MintingNow";
 import Upcoming from '../../components/homePage/upcoming/Upcoming';
 import PastMints from '../../components/homePage/past-mints/PastMints';
+import LauchSteps from '../../components/homePage/launch-steps/LaunchSteps';
 import "../../App.css"
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Header/>
       <MintingNow/>
       <Upcoming/>
+      <LauchSteps/>
       <PastMints/>
     </div>
   );
