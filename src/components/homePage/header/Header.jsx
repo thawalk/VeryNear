d59cd79c2a6095cdfc232996e3a4307fcbd3433c
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-08 20:47:22 +0800

    update header

--- src/components/homePage/header/Header.jsx
+++ src/components/homePage/header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './header.css';
 import ImageSlider from './ImageSlider';
+import logo from '../../../assets/LOGO.svg'
 import { SliderData } from './SliderData';
 
 
@@ -11,9 +12,8 @@ const HomePage = () => (
 const Header = () => (
     <div className="very-near__header section__padding" id="home">
       <div className="very-near__header-content">
-        <h1 style={{color: "#fff"}}>A NFT launchpad on</h1>
-        <h1 className="gradient__text">Near</h1>
-        <p>Get started with launching your first NFT project with VeryNear</p>
+        <h1 style={{color: "#fff"}}>A NFT launchpad on <span className="gradient__text" >NEAR</span></h1>
+        <p>Get started with launching your first NFT project with  <span className="gradient__text" >Very-near</span></p>
         <div className="very-near__header-content__input">
           <button type="button">Get Started</button>
         </div>
