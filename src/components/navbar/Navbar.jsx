1a8d71b9ccc68ad6d47f0246c00a497f6f09f5c7
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-10 02:32:21 +0800

    add toggle navbar options by page and route for logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom';
 import { RiMenuFill, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import { login, logout, testDeploy } from '../../utils'
@@ -8,36 +9,68 @@ import logo from '../../assets/LOGO.svg'
 function Navbar(props) {
   const [signedIn, setSignedIn] = props.signedIn
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+  const history = useHistory();
+
+
+  function checkPath(source) {
+    if (source === "home") {
+      setShowOptions(true)
+    }
+    else {
+      setShowOptions(false)
+    }
+  }
+
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      setShowOptions(true)
+    }
+  },
+    []
+  )
+
+  function checkSignedIn() {
+    // if (!window.walletConnection.isSignedIn()) {
+    //   login()
+    // }
+    // else {
+      let path = '/' 
+      history.push(path);
+    // }
+  }
 
   function Menu() {
     return (
       <>
-        {/* <p><NavLink exact to="/" activeClassName='active'>Home</NavLink></p>
-        <p><NavLink exact to="/minting-now" activeClassName='active'>Minting Now</NavLink></p>
-        <p><NavLink exact to="/upcoming" activeClassName='active'>Upcoming</NavLink></p>
-        <p><NavLink exact to="/past" activeClassName='active'>Past</NavLink></p>
-        <p><NavLink exact to="/about" activeClassName='active'>About</NavLink></p>
-        <p><NavLink exact to="/create" activeClassName='active'>Create</NavLink></p> */}
-
-        <p><NavLink exact to="/" activeClassName='active'>Home</NavLink></p>
-        <p><a href="#minting-now">Minting Now</a></p>
-        <p><a href="#upcoming">Upcoming</a></p>
-        <p><a href="#past-mints">Past Mints</a></p>
-        <p><a href="#about-us">About Us</a></p>
-        <p><NavLink exact to="/create" activeClassName='active'>Create</NavLink></p>
+        {showOptions ? <p><a href="#header">Home</a></p> : <p><NavLink exact to="/" onClick={() => checkPath("home")}>Home</NavLink></p>}
+        {
+          showOptions ?
+            <>
+              <p><a href="#minting-now">Minting Now</a></p>
+              <p><a href="#upcoming">Upcoming</a></p>
+              <p><a href="#past-mints">Past Mints</a></p>
+              <p><a href="#about-us">About Us</a></p>
+            </>
+            :
+            ""
+        }
+        <p><NavLink exact to="/create" onClick={() => checkPath("create")}>Create</NavLink></p>
       </>
     )
   }
 
 
   function setButton() {
-    return (signedIn
-      ? <>
-        {/* <div style={{ background: "#fff" }}> */}
-        <button type='button' style={{ background: "green" }} onClick={testDeploy}>Dashboard</button>
-        <button type='button' onClick={logout}>Sign Out</button>
-      </>
-      : <button type='button' style={{ background: "green" }} onClick={login}>Sign In</button>
+    return (
+      signedIn ?
+        <>
+          {/* <div style={{ background: "#fff" }}> */}
+          {/* <button type='button' style={{ background: "green" }} onClick={testDeploy}>Dashboard</button> */}
+          <button type='button' onClick={logout}>Disconnect Wallet</button>
+        </>
+        :
+        <button type='button' style={{ background: "green" }} onClick={login}>Connect Wallet</button>
     )
   }
 
@@ -45,7 +78,7 @@ function Navbar(props) {
     <div className="very-near__navbar">
       <div className="very-near__navbar-links">
         <div className="very-near__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} onClick={() => checkSignedIn()}/>
         </div>
         <div className="very-near__navbar-links_container">
           <Menu />
