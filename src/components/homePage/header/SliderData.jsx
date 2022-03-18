10c8ea3d8558a8fbc6d1ff1d46af108472581af6
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-18 23:24:36 +0800

    change to nft images

--- src/components/homePage/header/SliderData.jsx
+++ src/components/homePage/header/SliderData.jsx
@@ -1,6 +1,12 @@
 import nft_1 from '../../../assets/header-assets/nft_1.png'
 import nft_2 from '../../../assets/header-assets/nft_2.png'
 import nft_3 from '../../../assets/header-assets/nft_3.png'
+import nft_4 from '../../../assets/header-assets/nft_4.png'
+import nft_5 from '../../../assets/header-assets/nft_5.png'
+import nft_6 from '../../../assets/header-assets/nft_6.png'
+import nft_7 from '../../../assets/header-assets/nft_7.png'
+import nft_8 from '../../../assets/header-assets/nft_8.png'
+import nft_9 from '../../../assets/header-assets/nft_9.png'
 
 // export const SliderData = [
 //   {
@@ -29,5 +35,11 @@ export const SliderData =
   [
     nft_1,
     nft_2,
-    nft_3
+    nft_3,
+    nft_4,
+    nft_5,
+    nft_6,
+    nft_7,
+    nft_8,
+    nft_9
   ];
--- src/components/homePage/upcoming/imports.js
+++ src/components/homePage/upcoming/imports.js
@@ -1,10 +1,28 @@
 import nft_1 from '../../../assets/upcoming-assets/nft_1.png'
 import nft_2 from '../../../assets/upcoming-assets/nft_2.png'
 import nft_3 from '../../../assets/upcoming-assets/nft_3.png'
+import nft_4 from '../../../assets/upcoming-assets/nft_4.png'
+import nft_5 from '../../../assets/upcoming-assets/nft_5.png'
+import nft_6 from '../../../assets/upcoming-assets/nft_6.png'
+import nft_7 from '../../../assets/upcoming-assets/nft_7.png'
+import nft_8 from '../../../assets/upcoming-assets/nft_8.png'
+import nft_9 from '../../../assets/upcoming-assets/nft_9.png'
 
 export default
+[
   [
     nft_1,
     nft_2,
     nft_3
-  ];
+  ],
+  [
+    nft_4,
+    nft_5,
+    nft_6
+  ],
+  [
+    nft_7,
+    nft_8,
+    nft_9
+  ]
+];
