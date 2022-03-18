80b6f502393350ba2acbcfa92080a73bf2c426dc
Tharun Adhi Narayan <tharun.narayan@workato.com>
2022-03-18 23:21:29 +0800

    image slider in header component

--- src/components/homePage/header/Header.jsx
+++ src/components/homePage/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './header.css';
-import ImageSlider from './ImageSlider';
+import ImageSlider from '../common-components/image-slider/ImageSlider';
 import { SliderData } from './SliderData';
 import { useHistory } from "react-router-dom";
 
@@ -9,36 +9,29 @@ const HomePage = () => (
   <div style={{ backgroundColor: "#6DABF8", height: "1000px" }}>HomePage </div>
 );
 
-// const Header = ({ openDialog }) => (
-
-function Header({ showOptionsFunc, currentUser, login }) {
+function Header({ showOptionsFunc }) {
   const history = useHistory();
-  function checkSignedIn() {
-    if (!currentUser) {
-      login()
-    }
-    else {
-      showOptionsFunc(false)
-      let path = '/create' 
-      history.push(path);
-    }
+  function createPage() {
+    showOptionsFunc(false)
+    let path = '/create'
+    history.push(path);
   }
 
 
   return (
-      <div className="very-near__header section__padding" id="header">
-        <div className="very-near__header-content">
-          <h1 style={{ color: "#fff" }}>A NFT launchpad on <span className="gradient__text" >NEAR</span></h1>
-          <p>Get started with launching your first NFT project with  <span className="gradient__text" >VeryNear</span></p>
-          <div className="very-near__header-content__input">
-            <button type="button" onClick={checkSignedIn}>Get Started</button>
-          </div>
+    <div className="very-near__header section__padding" id="header">
+      <div className="very-near__header-content">
+        <h1 style={{ color: "#fff" }}>A NFT launchpad on <span className="gradient__text" >NEAR</span></h1>
+        <p>Get started with launching your first NFT project with  <span className="gradient__text" >VeryNear</span></p>
+        <div className="very-near__header-content__input">
+          <button type="button" onClick={createPage}>Get Started</button>
         </div>
-        <div className='very-near__header-image'>
-          <ImageSlider slides={SliderData} />
-        </div>
-        {/* <HomePage/> */}
       </div>
+      <div className='very-near__header-image'>
+        <ImageSlider slides={SliderData} showOptionsFunc={showOptionsFunc}/>
+      </div>
+      {/* <HomePage/> */}
+    </div>
   )
 };
 
